8201f9d8e187b22b59387d2646970bf9d563323f
Nate Harada <nharada1@gmail.com>
2015-06-03 13:38:28 -0400

    Updated and added OSX stuff

---
+++ osx/set-defaults.sh
@@ -0,0 +1,26 @@
+# Sets reasonable OS X defaults.
+#
+# Or, in other words, set shit how I like in OS X.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+# which then grabbed from:
+#   https://raw.githubusercontent.com/holman/dotfiles/master/osx/set-defaults.sh
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
