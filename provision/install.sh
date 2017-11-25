a2152f4fc8b11ef2aae8a8c721bebedbb9d89876
Nate Harada <nharada1@gmail.com>
2017-11-25 11:51:52 -0800

    Big provision updates

--- provision/install.sh
+++ provision/install.sh
@@ -8,26 +8,26 @@ bare_minimums () {
     sudo -v
 
     # Update packages
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
+    sudo apt update -y
+    sudo apt upgrade -y
 
     # Install bare essentials
-    sudo apt-get install -y openssh-server default-jre 
+    sudo apt install -y openssh-server default-jre 
 
     # Install tmux
-    sudo apt-get install -y tmux
+    sudo apt install -y tmux
 
     # Install git
-    sudo apt-get install -y vim git-core
+    sudo apt install -y vim git-core
     git config --global user.name "Nate Harada"
     git config --global user.email "nharada1@gmail.com"
 
     # Install zsh/oh-my-zsh
-    sudo apt-get install -y zsh
+    sudo apt install -y zsh
     wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 
     # Install vim/vundle
-    sudo apt-get install -y vim
+    sudo apt install -y vim
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
     # Install dotfiles
@@ -35,18 +35,12 @@ bare_minimums () {
     bash ~/.dotfiles/script/bootstrap
 }
 
-gnome_machine () {
-    # Context menu terminal
-    sudo apt-get install -y nautilus-open-terminal  
-    # Flash install
-    sudo apt-get install -y flashplugin-nonfree
-}
-
-build_machine () {
+dev() {
     # Build tools
-    sudo apt-get install -y build-essential  
+    sudo apt install -y build-essential  
+
     # Augment tmux with information console
-    sudo apt-get install -y cmake
+    sudo apt install -y cmake
     git clone https://github.com/thewtex/tmux-mem-cpu-load.git ~/Downloads/tmux-mem-cpu-load
     cd ~/Downloads/tmux-mem-cpu-load
     cmake .
@@ -55,27 +49,23 @@ build_machine () {
     cd -
 }
 
-scikit_dev_machine () {
+ml() {
     # Python3
-    sudo apt-get install -y python3 python3-numpy python3-scipy python3-nose
+    sudo apt install -y python3 python3-numpy python3-scipy python3-nose
 
     # Python2 packages
-    sudo apt-get install -y python-numpy python-scipy python-nose
+    sudo apt install -y python-numpy python-scipy python-nose
 }
 
-python_dev_machine() {
-    # Python3
-    sudo apt-get install -y python3 python3-pip
-}
+desktop() {
+    # Update the sources list and add the GPG keys for the non-free PPAs
+    curl https://repogen.simplylinux.ch/txt/artful/sources_64c0fd6a1e3342c3239f00b4d6818507d73a88ec.txt | sudo tee /etc/apt/sources.list
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6A68F637
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
 
-michart_machine () {
-    # Install VPN
-    sudo apt-get install -y network-manager-openconnect-gnome
-
-    # Install Citrix. This will require the icaclient deb package. 
-    sudo dpkg -i ./icaclient_13.1.0.285639_amd64.deb
-    sudo apt-get install -f -y
-    sudo dpkg -i ./icaclient_13.1.0.285639_amd64.deb
+    # Update the whole thing
+    sudo apt update
 }
 
 # Run install, extras based on command line arguments
